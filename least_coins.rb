b0610c23eea71ac30a2460e3b74ca0746b955e31
rabiah1 <rabiahn01@gmail.com>
2018-08-09 15:09:34 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,19 @@
 #write out your code here
+x = 189
 
-def least_coins(cents)
+def least_coins(x)
+  a = x/25
+b = x%25
+  c = b/10
+d = b%10
+  e = d/5 
+f = d%5
+  g = f+  1 
+  
+  final = a + c + e + g 
 
-#Code your answer here!
+puts final 
 
-end
+end 
+
+coins = 143
