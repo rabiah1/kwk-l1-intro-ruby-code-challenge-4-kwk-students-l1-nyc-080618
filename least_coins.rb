19a5a47a7db040b2bf4d792128f0922cf3c7288f
rabiah1 <rabiahn01@gmail.com>
2018-08-09 15:13:55 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,20 +2,15 @@
 x = 189
 
 def least_coins(x)
-  a = x/25
-b = x%25
-  c = b/10
-d = b%10
-  e = d/5 
-f = d%5
-  g = f+  1 
-  
-  final = a + c + e + g 
+    a = x/25
+  b = x%25
+    c = b/10
+  d = b%10
+    e = d/5
+  f = d%5
+    g = f/1
+  final = a + c + e + g
+  puts final
+end
 
-puts final 
-
-end 
-
-coins = 143
-
-least_coins (143)
+least_coins(x)
